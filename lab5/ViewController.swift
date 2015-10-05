c137aad972ccf974e5adf2ea4e952c5d714cbe2b
Philip Tibbetts <Phil@cs-eduroam-55.cs.wisc.edu>
2015-10-05 14:25:10 -0500

    Initial Commit

---
+++ lab5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lab5
+//
+//  Created by Philip Tibbetts on 10/5/15.
+//  Copyright (c) 2015 Philip Tibbetts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
